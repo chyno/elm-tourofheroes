1fab74d5cf38337d2fea9e5391f5915bd5166e92
John <jchynowe1@yahoo.com>
2017-12-04 13:44:50 -0500

    baseline

--- app/index.js
+++ app/index.js
@@ -1,12 +1,12 @@
 'use strict';
 
-require('ace-css/css/ace.css');
-require('font-awesome/css/font-awesome.css');
+//require('ace-css/css/ace.css');
+//require('font-awesome/css/font-awesome.css');
 
 // Require index.html so it gets copied to dist
 require('./index.html');
 
-var Elm = require('./Main.elm');
+var Elm = require('./src/Main.elm');
 var mountNode = document.getElementById('main');
 
 // The third value on embed are the initial values for incomming ports into Elm
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 var path = require("path");
+var elmSource = __dirname + '/app'
 
 module.exports = {
+  context: __dirname + "/app",
   entry: {
     app: [
-      './src/index.js'
+      './index.js'
     ]
   },
 
@@ -18,26 +20,29 @@ module.exports = {
         test: /\.(css|scss)$/,
         use: [
           'style-loader',
-          'css-loader',
+          'css-loader'
         ]
       },
       {
         test:    /\.html$/,
         exclude: /node_modules/,
-        loader:  'file-loader?name=[name].[ext]',
+        loader:  'file-loader?name=[name].[ext]'
       },
       {
         test:    /\.elm$/,
         exclude: [/elm-stuff/, /node_modules/],
         loader:  'elm-webpack-loader?verbose=true&warn=true',
+        options: {
+          cwd: elmSource
+        }
       },
       {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
       },
       {
         test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'file-loader',
+        loader: 'file-loader'
       },
     ],
 
