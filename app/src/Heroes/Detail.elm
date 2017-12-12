b12f57179cefff9eb8ea9b156be6b87d90327c5c
John <jchynowe1@yahoo.com>
2017-12-12 10:45:41 -0500

    add new Detail page

---
+++ app/src/Heroes/Detail.elm
@@ -0,0 +1,28 @@
+module Heroes.Detail exposing (..)
+import Html exposing (..)
+import Models exposing (..)
+import Msgs exposing (Msg)
+import Html.Events exposing (onClick)
+
+{-
+<div *ngIf="hero">
+	<h2>{{hero.name}} details!</h2>
+	<div>
+		<label>id: </label>{{hero.id}}</div>
+	<div>
+		<label>name: </label>
+		<input [(ngModel)]="hero.name" placeholder="name"/>
+	</div>
+	<button (click)="goBack()">Back</button>
+</div>
+-}
+detailView :  Hero -> Html Msg
+detailView hero =
+  div
+  []
+  [
+    h2 [] [text (hero.name ++ " details!")]
+    , div [] [ label [] [text "id :"] , text hero.id ]
+    , div [] [ label [] [text "name:"], input [] [] ]
+    , button [ onClick Msgs.GoBack ] [text "Back"]
+  ]
